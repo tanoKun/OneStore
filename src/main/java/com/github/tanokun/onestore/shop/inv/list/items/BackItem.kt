9a48846f9a5defd85c98a427d6fdc9a16d0afb93
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2022-09-17 23:31:15 +0900

    add: ショップリストのバックボタン

---
+++ src/main/java/com/github/tanokun/onestore/shop/inv/list/items/BackItem.kt
@@ -0,0 +1,20 @@
+package com.github.tanokun.onestore.shop.inv.list.items
+
+import de.studiocode.invui.gui.impl.PagedGUI
+import de.studiocode.invui.item.ItemProvider
+import de.studiocode.invui.item.builder.ItemBuilder
+import de.studiocode.invui.item.impl.controlitem.PageItem
+import org.bukkit.Material
+
+class BackItem : PageItem(false) {
+
+    override fun getItemProvider(gui: PagedGUI): ItemProvider {
+        val builder = ItemBuilder(Material.ARROW)
+            .setDisplayName("§7前のページに戻る")
+            .addLoreLines("§7§e${gui.currentPageIndex} ページ")
+
+        if (!gui.hasPageBefore()) return ItemBuilder(Material.BLACK_STAINED_GLASS_PANE)
+
+        return builder
+    }
+}
