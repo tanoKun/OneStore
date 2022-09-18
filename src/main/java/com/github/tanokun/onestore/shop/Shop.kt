0fd185689465b07edcd93a1e568d4fc9c1181c54
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2022-09-18 16:20:06 +0900

    add: 売値、買値の変動の追加

--- src/main/java/com/github/tanokun/onestore/shop/Shop.kt
+++ src/main/java/com/github/tanokun/onestore/shop/Shop.kt
@@ -1,6 +1,41 @@
 package com.github.tanokun.onestore.shop
 
+import net.objecthunter.exp4j.ExpressionBuilder
 import org.bukkit.inventory.ItemStack
+import kotlin.math.roundToInt
 
-class Shop(val id: String, val product: ItemStack, var basePrice: Int, var stock: Int) {
+class Shop(val id: String, val product: ItemStack, var basePrice: Int, stock: Int, var change: Int) {
+    var sellExpressionPlus: String = "n + n * 0.01"
+
+    var sellExpressionSubtract: String = "n - n * 0.01"
+
+    var stock: Int = stock
+        set(value) {
+            difference =+ stock - value
+            if (difference < -change) {
+                sellPrice = ExpressionBuilder(sellExpressionPlus).variables("n")
+                    .build().setVariable("n", sellPrice.toDouble()).evaluate().roundToInt()
+
+                difference = 0
+            }
+
+            if (difference > change) {
+                sellPrice = ExpressionBuilder(sellExpressionSubtract).variables("n")
+                    .build().setVariable("n", sellPrice.toDouble()).evaluate().roundToInt()
+
+                difference = 0
+            }
+
+
+            field = value
+        }
+    var buyPrice = 0
+    var sellPrice = 0
+
+    var difference = 0
+
+    init {
+        buyPrice = (basePrice * 1.05).roundToInt()
+        sellPrice = (basePrice / 1.05).roundToInt()
+    }
 }
