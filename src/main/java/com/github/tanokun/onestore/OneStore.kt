6cf4eaad8b4ba3928e15362ed31b80d7285d7a44
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2022-09-18 09:13:24 +0900

    add: db, shopManager

--- src/main/java/com/github/tanokun/onestore/OneStore.kt
+++ src/main/java/com/github/tanokun/onestore/OneStore.kt
@@ -1,5 +1,9 @@
 package com.github.tanokun.onestore
 
+import com.github.tanokun.onestore.commands.StoreCommand
+import com.github.tanokun.onestore.listeners.Test
+import com.github.tanokun.onestore.shop.ShopManager
+import com.github.tanokun.onestore.util.database.Database
 import dev.jorel.commandapi.CommandAPI
 import jp.jyn.jecon.Jecon
 import org.bukkit.Bukkit
@@ -9,14 +13,33 @@ lateinit var plugin: OneStore
 
 lateinit var jecon: Jecon
 
+lateinit var db: Database
+
+lateinit var shopManager: ShopManager
+
 class OneStore : JavaPlugin() {
     override fun onEnable() {
         plugin = this
         jecon = Bukkit.getPluginManager().getPlugin("Jecon") as Jecon
+        db = Database()
+        shopManager = ShopManager()
 
         CommandAPI.onEnable(plugin)
+
+        registerCommands()
+        registerListeners()
     }
 
     override fun onDisable() {
+        db.close()
+}
+
+    private fun registerCommands() {
+        StoreCommand()
+    }
+
+
+    private fun registerListeners() {
+        Bukkit.getPluginManager().registerEvents(Test(), this)
     }
 }
