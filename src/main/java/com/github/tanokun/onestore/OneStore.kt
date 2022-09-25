c1c4ac521f9ef59607ddd27cb29b60f25b92cc7a
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2022-09-25 16:50:38 +0900

    add: register系を追加

--- src/main/java/com/github/tanokun/onestore/OneStore.kt
+++ src/main/java/com/github/tanokun/onestore/OneStore.kt
@@ -1,11 +1,14 @@
 package com.github.tanokun.onestore
 
 import com.github.tanokun.onestore.commands.StoreCommand
-import com.github.tanokun.onestore.listeners.Test
+import com.github.tanokun.onestore.commands.logger.OstoreAppender
+import com.github.tanokun.onestore.listeners.OperationClickListener
 import com.github.tanokun.onestore.shop.ShopManager
 import com.github.tanokun.onestore.util.database.Database
 import dev.jorel.commandapi.CommandAPI
 import jp.jyn.jecon.Jecon
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.core.Logger
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -23,17 +26,27 @@ class OneStore : JavaPlugin() {
         jecon = Bukkit.getPluginManager().getPlugin("Jecon") as Jecon
         db = Database()
         shopManager = ShopManager()
+        shopManager.loadHologram()
 
         CommandAPI.onEnable(plugin)
 
         registerCommands()
         registerListeners()
+
+        logger()
     }
 
     override fun onDisable() {
         shopManager.shops.forEach {
             db.saveShop(it.value)
         }
+
+        shopManager.holograms.forEach {
+            db.saveHologramLocation(it.key, it.value)
+        }
+
+        val log: Logger = LogManager.getRootLogger() as Logger
+        log.removeAppender(OstoreAppender)
 
         db.close()
 }
@@ -44,6 +57,13 @@ class OneStore : JavaPlugin() {
 
 
     private fun registerListeners() {
-        Bukkit.getPluginManager().registerEvents(Test(), this)
+        Bukkit.getPluginManager().registerEvents(OperationClickListener(), this)
+    }
+
+    private fun logger() {
+        val log: Logger = LogManager.getRootLogger() as Logger
+        log.addAppender(OstoreAppender)
     }
 }
+
+infix fun <T> Boolean.then(other: T) = if (this) other else null
