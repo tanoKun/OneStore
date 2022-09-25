b9f358b347aa9f78dc5fc97c497ceaa7e5b82f68
tanokun <azsxdcfvgbhnjmklqawsedrf0@gmail.com>
2022-09-25 16:54:22 +0900

    fix: 金額が変動しないバグを修正

--- src/main/java/com/github/tanokun/onestore/shop/Shop.kt
+++ src/main/java/com/github/tanokun/onestore/shop/Shop.kt
@@ -2,40 +2,48 @@ package com.github.tanokun.onestore.shop
 
 import net.objecthunter.exp4j.ExpressionBuilder
 import org.bukkit.inventory.ItemStack
-import kotlin.math.roundToInt
+import kotlin.math.abs as abs
 
-class Shop(val id: String, val product: ItemStack, var basePrice: Int, stock: Int, var change: Int) {
-    var sellExpressionPlus: String = "n + n * 0.01"
+class Shop(val id: String, val product: ItemStack, var basePrice: Int, var stock: Int, var change: Int, var increase: Boolean) {
+    var expressionPlus: String = "n + n * 0.01"
 
-    var sellExpressionSubtract: String = "n - n * 0.01"
+    var expressionSubtract: String = "n - n * 0.01"
 
-    var stock: Int = stock
-        set(value) {
-            difference =+ stock - value
-            if (difference < -change) {
-                sellPrice = ExpressionBuilder(sellExpressionPlus).variables("n")
-                    .build().setVariable("n", sellPrice.toDouble()).evaluate().roundToInt()
-
-                difference = 0
-            }
-
-            if (difference > change) {
-                sellPrice = ExpressionBuilder(sellExpressionSubtract).variables("n")
-                    .build().setVariable("n", sellPrice.toDouble()).evaluate().roundToInt()
-
-                difference = 0
-            }
-
-
-            field = value
-        }
-    var buyPrice = 0
-    var sellPrice = 0
+    var buyPrice = 0.0
+    var sellPrice = 0.0
 
     var difference = 0
 
     init {
-        buyPrice = (basePrice * 1.05).roundToInt()
-        sellPrice = (basePrice / 1.05).roundToInt()
+        buyPrice = basePrice * 1.05
+        sellPrice = basePrice / 1.05
+    }
+
+    fun addStock(stock: Int) {
+        this.stock += stock
+        this.difference += stock
+
+        while (difference >= change) {
+            sellPrice = ExpressionBuilder(expressionSubtract).variables("n")
+                .build().setVariable("n", sellPrice).evaluate()
+
+            buyPrice = ExpressionBuilder(expressionSubtract).variables("n")
+                .build().setVariable("n", buyPrice).evaluate()
+            difference -= change
+        }
+    }
+
+    fun removeStock(stock: Int) {
+        this.stock -= stock
+        this.difference -= stock
+
+        while (abs(difference) >= change) {
+            buyPrice = ExpressionBuilder(expressionPlus).variables("n")
+                .build().setVariable("n", buyPrice).evaluate()
+
+            sellPrice = ExpressionBuilder(expressionPlus).variables("n")
+                .build().setVariable("n", sellPrice).evaluate()
+            difference += change
+        }
     }
 }
